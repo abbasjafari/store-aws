44fad7799a1fe14ebd2863bc295257c4996356dd
ali karimi <alikarimi.8765@gmail.com>
2022-08-11 13:16:41 +0200

    config-basket

--- basket/src/main/java/com/hello/basket/BasketApplication.java
+++ basket/src/main/java/com/hello/basket/BasketApplication.java
@@ -1,5 +1,7 @@
 package com.hello.basket;
 
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
--- basket/src/main/java/com/hello/basket/PropertiesController.java
+++ basket/src/main/java/com/hello/basket/PropertiesController.java
@@ -1,21 +1,17 @@
 package com.hello.basket;
 
-import com.netflix.discovery.converters.Auto;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PropertiesController {
-    @Autowired
-    private ConfigClientAppConfiguration properties;
 
-    @Value("environment")
+    @Value("${some.other.property}")
     String environment;
 
     @RequestMapping
-    public String printConfig(){
+    public String printConfig() {
         return environment;
     }
 
